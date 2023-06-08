3b942a56046c3b2bd3c09a58aeddb2d167d3bcff
Fatema Bohra <fatema.bohra2003@gmail.com>
2023-06-08 17:14:09 -0300

    Add the smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,65 @@
+const Clarifai = require('clarifai');
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '739feb1cb0dc4987b9429e217a253f78';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'fatema';
+    const APP_ID = 'test';
+    // Change these to whatever model and image URL you want to use
+    // const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions
+}
+
+const handleApiCall = (req, res) => {
+    const MODEL_ID = 'face-detection';
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(req.body.input))
+        .then(response => response.json())
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with api'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0].entries);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+};
